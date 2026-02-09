3c4d192cbff5f7cead3a7a1bcdbbacf46fdd51c0
amcjulian <jolianmc@hotmail.com>
2026-02-08 20:03:55 -0500

    calculadora_laboratorio

---
+++ main.py
@@ -0,0 +1,9 @@
+
+print("---------------------------------------------")
+print("*Ingrese dos numeros a sumar, si elnumero que\n ingresa es decimal coloque punto en vez de coma")
+print("---------------------------")
+num1 = input("ingrese primer numero \n")
+num2 = input("ingrese segundo numero\n")
+resultado = (float(num1) + float(num2))
+print("El resultado es :", resultado)
+print("---------------------------")
